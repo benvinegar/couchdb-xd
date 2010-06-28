bd7f3f989765615e55f4d6aab3225134a34eab9e
bentlegen <ben@benlog.org>
2010-06-27 22:02:49 -0700

    PUT document method

--- _attachments/couchdb.js
+++ _attachments/couchdb.js
@@ -1,8 +1,21 @@
+/**
+ *---------------------------------------------------------------
+ * CouchDB cross-domain client library
+ *---------------------------------------------------------------
+ *
+ * Example Usage:
+ *  var server = new Couch.Server('http://myserver.com');
+ *  var db = server.getDatabase('test');
+ *  db.get('document-name', function(doc) { alert(doc); });
+ *
+ * Copyright Ben Vinegar http://benv.ca
+ */
+
 window.Couch = (function() {
   var Couch = {};
 
-  Couch.init = function(host) {
-    Couch.host = host;
+  Couch.Server = function(host) {
+    this.host = host;
 
     var script = document.createElement('script');
     script.src = host + '/couchdb-xd/_design/couchdb-xd/lib/pmdxr-client.js';
@@ -10,19 +23,40 @@ window.Couch = (function() {
     body.appendChild(script);
   };
 
-  Couch.Database = function(name) {
-    this.url = Couch.host + '/' + name;
+  Couch.Server.prototype = {
+    getDatabase: function(name) {
+      return new Couch.Database(this, name);
+    }
+  };
+
+  Couch.Database = function(server, name) {
+    this.name = name;
+    this.url = server.host + '/' + name;
   }
 
-  Couch.Database.prototype.get = function(name, callback) {
-    var self = this;
-    pmxdr.request({
-      uri: self.url + '/' + name,
-      callback: function(response) {
-        callback(response.data);
-      }
-    });
-  }
+  Couch.Database.prototype = {
+    get: function(name, callback) {
+      var self = this;
+      pmxdr.request({
+        uri: self.url + '/' + name,
+        callback: function(response) {
+          callback(response.data);
+        }
+      });
+    },
+
+    put: function(name, data, callback) {
+      var self = this;
+      pmxdr.request({
+        uri: self.url + '/' + name,
+        method: "PUT",
+        data: JSON.stringify(data),
+        callback: function(response) {
+          callback(response.data);
+        }
+      });
+    }
+  };
 
   return Couch;
 })();

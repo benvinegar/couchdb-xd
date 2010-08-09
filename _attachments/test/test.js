3df5f7a6b6938f189d7b83f5cba24d13f46f972d
benvinegar <ben@benlog.org>
2010-08-08 17:22:57 -0700

    Simple fixture-loader to reduce test duplication

--- _attachments/test/test.js
+++ _attachments/test/test.js
@@ -1,11 +1,17 @@
 Couch.init(function() {
   var server = new Couch.Server('http://localhost:5984', 'admin', 'pass');
 
+  function loadFixtures(callback) {
+    db.put('sample-record', { hello: 'world' }, callback);
+  }
+
   function setup(callback) {
     // Recreate the test server each test
     server.get(server, 'couchdb_xd_test', function(response) {
       server.destroy(server, 'couchdb_xd_test', function(response) {
-        server.create(server, 'couchdb_xd_test', callback);
+        server.create(server, 'couchdb_xd_test', function() {
+          loadFixtures(callback);
+        });
       });
     });
   }
@@ -26,24 +32,20 @@ Couch.init(function() {
   test('can fetch a record', 1, function() {
     stop();
     setup(function() {
-      db.put('new-record', { hello: 'world' }, function() {
-        db.get('new-record', function(resp) {
-          start();
-          // TODO: normalize '_id' vs 'id'
-          equals(resp._id, 'new-record');
-        });
+      db.get('sample-record', function(resp) {
+        start();
+        // TODO: normalize '_id' vs 'id'
+        equals(resp._id, 'sample-record');
       });
     });
   });
 
   test('can delete a record', 1, function() {
     stop();
-    setup(function() {
-      db.put('new-record', { hello: 'world' }, function(resp) {
-        db.destroy('new-record', { rev: resp.rev }, function(resp) {
-          start();
-          equals(resp.ok, true);
-        });
+    setup(function(resp) {
+      db.destroy('sample-record', { rev: resp.rev }, function(resp) {
+        start();
+        equals(resp.ok, true);
       });
     });
   });
